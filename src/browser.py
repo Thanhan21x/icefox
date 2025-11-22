430a5010a33275f70f542376795ae18f98271b2f
Thanh Nhan <thanh.nhan.21@proton.me>
2025-11-22 15:39:49 +0700

    add scroll text

--- src/browser.py
+++ src/browser.py
@@ -100,6 +100,10 @@ class URL:
 
 WIDTH, HEIGHT = 800, 600
 
+HSTEP, VSTEP = 13, 18
+
+SCROLL_STEP = 100
+
 class Browser:
     def __init__(self):
         self.window = tkinter.Tk()
@@ -109,22 +113,37 @@ class Browser:
             height=HEIGHT
         )
         self.canvas.pack()
+        self.scroll = 0
+        self.window.bind("<Down>", self.scrolldown)
 
     def load(self, url):
-
         body = url.request()
-
         text = lex(body)
+        self.display_list = layout(text)
+        
+        self.draw()
 
-        HSTEP, VSTEP = 13, 18
-        cursor_x, curosr_y = HSTEP, VSTEP
-        for c in text:
-            self.canvas.create_text(cursor_x, curosr_y, text=c)
-            cursor_x += HSTEP
-            if cursor_x >= WIDTH - HSTEP:
-                curosr_y += VSTEP
-                cursor_x = HSTEP
+    def draw(self):
+        self.canvas.delete("all")
 
+        for x,y,c in self.display_list:
+            self.canvas.create_text(x, y - self.scroll, text=c)
+
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
+
+def layout(text):
+    display_list = []
+    cursor_x, cursor_y = HSTEP, VSTEP
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+        cursor_x += HSTEP
+        if cursor_x >= WIDTH - HSTEP:
+            cursor_y += VSTEP
+            cursor_x = HSTEP
+    
+    return display_list
 
 def lex(body):
     text = ""
@@ -169,6 +188,5 @@ if __name__ == "__main__":
         tkinter.mainloop()
     elif len(sys.argv) == 1: 
         Browser().load(URL(default_url))
-
 
 
